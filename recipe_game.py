e16dc6a66abcfbff8b92fd381203d9ad026bbe28
vansh malhotras <vansh.malhotra@seaflux.tech>
2024-01-17 18:39:13 +0530

    Doene

--- recipe_game.py
+++ recipe_game.py
@@ -12,29 +12,37 @@ def add_to_recipe(recipe_id, recipe_name, recipe_type, recipe_desc):
     recipe_item.append(new_recipe)
     print("Recipe Added Succesfully")
     
-def update_recipe(recipe_id, recipe_name, recipe_type, recipe_desc):
+def update_recipe(recipe_id):
     global recipe_item
+    
     for recipe in recipe_item:
-        if recipe['id'] == update_id:
+        if recipe['id'] == recipe_id:
+            recipe_name=input("Enter New Name: ")
+    
+            
+            recipe_type=input("Enter New Type: ")
+            
+        
+            recipe_desc=input("Enter New Desc: ")
             recipe['name'] = recipe_name
             recipe['type'] = recipe_type
             recipe['desc'] = recipe_desc
             print(f"Recipe with ID {update_id} updated successfully")
             return
-    
-    print(f"Recipe with ID {update_id} not found")
+        else:
+            print(f"Recipe with ID {update_id} not found")
 temp=1
 while temp: 
         
     temp= int(input(" \n 0) Exit \n 1) Start \n Enter Value: "))  
     recipe_no = 1
     flag=1
-    print("Welcome to Recipe App\n")
+    
     if temp==1:
 
         while flag:
             recipe_functions = int(input("\n Recipe App Functions \n\n 1)Add Recipe \n\n 2) Update Recipe \n\n 3) Delete Recipe \n\n 4) View Recipe \n\n 5) Exit \n Enter Value:"))
-
+            print("Welcome to Recipe App")
             if recipe_functions == 1:
                 recipe_function_id = recipe_no
                 recipe_no+=1
@@ -55,25 +63,32 @@ while temp:
                 print("Enter the id you want to update: ")
                 update_id=int(input())
                 
-                recipe_name_update=input("Enter New Name: ")
-                
-                
-                recipe_type_update=input("Enter New Type: ")
-                
-                
-                recipe_desc_update=input("Enter New Desc: ")
-                
-                update_recipe(update_id,recipe_name_update,recipe_type_update,recipe_desc_update)
+             
+                if len(recipe_item)==0:
+                    print(f"Recipe with ID {update_id} not found")
+                else:
+                    print(f"Recipe with ID {update_id} not found")
+                       
+                update_recipe(update_id)
                 
             
             elif recipe_functions==3:
                 
                 print("Enter the id you want to delete: ")
                 delete_id=int(input())
+              
+                if len(recipe_item)==0:
+                     print(f"Recipe with ID {delete_id} not found")
                 
+                    
                 for recipe in recipe_item:
                     if delete_id==recipe['id']:
                         recipe_item.remove(recipe)
+                        print(f"Recipe with ID {delete_id} updated successfully")
+                    else:
+                       
+                        print(f"Recipe with ID {delete_id} not found")
+                
                 
                 
             elif recipe_functions == 4:
@@ -90,7 +105,7 @@ while temp:
                 flag=0
         
     else:
-        temp=0
+        break
             
     
     
